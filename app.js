fd7e60a4269b93a0bcbccb802f32599dff7c7153
Jaya <jaya.jhs21@gmail.com>
2020-07-18 13:00:35 +0530

    Fixes

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const apiKey = '29f3c322a468a721b77c65891490a108'
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.set('view engine', 'ejs')
-
 app.get('/', function (req, res) {
-    res.render('index');
+  res.render('index');
 });
+
+app.set('view engine', 'ejs')
 
 app.listen(3000, function () {
   console.log('Weather app listening on port 3000!')
 })
 
-
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
   needle.request('post', url, { city }, function (err, response) {
+    console.log(response.body);
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
